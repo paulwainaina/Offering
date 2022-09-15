e93403cf3ddd39798bba214945c2a4b372826512
paulwainaina <paulwainaina36@gmail.com>
2022-09-15 10:39:38 +0300

    index page

--- controller/front.go
+++ controller/front.go
@@ -1,6 +1,23 @@
 package controller
 
-import "net/http"
+import (
+	"io/ioutil"
+	"net/http"
+	"text/template"
+)
+
+var (
+	tpl * template.Template
+)
+
+func init(){
+	tpl=template.Must(template.ParseGlob("templates/*.html"))
+}
+
+type Page struct{
+	Body []byte
+	Title string
+}
 
 type FrontController struct{
 
@@ -13,9 +30,36 @@ func NewFrontController()(*FrontController){
 func (front FrontController)ServeHttp(w http.ResponseWriter,r * http.Request){
 	switch r.URL.Path{
 	case "/":{
-		w.Write([]byte("welcome"))
+		front.IndexPage(w)
 	}
-	
+
+	}
+}
+
+func (front FrontController) LoadPage(file string)(*Page,error){
+	body,err:=ioutil.ReadFile(file)
+	if err!=nil{
+		return nil,err
+	}
+	return &Page{Body:body},nil
+}
+
+func (front FrontController) IndexPage(w http.ResponseWriter){
+	file:="index.html"
+	filePath:="templates/"+file
+	pageName:="Home Page"
+	page,err:=front.LoadPage(filePath)
+	if err !=nil{
+		page=&Page{Title:pageName}
+	}
+	page.Title=pageName
+	front.RenderTemplate(w,file,page)
+}
+
+func (front FrontController)RenderTemplate(w http.ResponseWriter,file string,page *Page){
+	err:=tpl.ExecuteTemplate(w,file,page)
+	if err !=nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
 	}
 }
 
--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+
+
 func main(){
 	err:=godotenv.Load(".env")
 	if err!=nil{
